f8fe91aa587cb6c5b6240e533275d5872d86ca3b
William Boafo <boafowill@gmail.com>
2023-07-05 11:31:05 -0400

    Add :id routes for Films, Planets, and Characters

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const model = require('../model/model');
 
 //route for api/planets
-router.get('/planets', async (req, res)=> {
+router.get('/planets', async (req, res) => {
     try {
         const data = await model.Planets.find();
         res.json(data)
@@ -12,7 +12,7 @@ router.get('/planets', async (req, res)=> {
     }
 })
 
-router.get('/characters', async (req, res)=> {
+router.get('/characters', async (req, res) => {
     try {
         const data = await model.Characters.find();
         res.json(data)
@@ -21,7 +21,7 @@ router.get('/characters', async (req, res)=> {
     }
 })
 
-router.get('/films', async (req, res)=> {
+router.get('/films', async (req, res) => {
     try {
         const data = await model.Films.find();
         res.json(data)
@@ -30,35 +30,50 @@ router.get('/films', async (req, res)=> {
     }
 })
 
-router.get('/characters/:id', (req, res)=> {
+router.get('/characters/:id', async (req, res) => {
+    try {
+        const data = await model.Characters.findById(req.params.id)
+        res.json(data)
+    } catch(err) {
+        res.status(500).json({message: err.message})
+    }
+})
+
+router.get('/films/:id', async (req, res) => {
+    try {
+        const data = await model.Characters.findById(req.params.id)
+        res.json(data)
+    } catch(err) {
+        res.status(500).json({message: err.message})
+    }
+})
+
+router.get('/planets/:id', async (req, res) => {
+    try {
+        const data = await model.planets.findById(req.params.id)
+        res.json(data)
+    } catch(err) {
+        res.status(500).json({message: err.message})
+    }
+})
+
+router.get('/films/:id/characters', (req, res) => {
     res.send('Get All Planets')
 })
 
-router.get('/films/:id', (req, res)=> {
+router.get('/films/:id/planets', (req, res) => {
     res.send('Get All Planets')
 })
 
-router.get('/planets/:id', (req, res)=> {
+router.get('/characters/:id/films', (req, res) => {
     res.send('Get All Planets')
 })
 
-router.get('/films/:id/characters', (req, res)=> {
+router.get('/planets/:id/films', (req, res) => {
     res.send('Get All Planets')
 })
 
-router.get('/films/:id/planets', (req, res)=> {
-    res.send('Get All Planets')
-})
-
-router.get('/characters/:id/films', (req, res)=> {
-    res.send('Get All Planets')
-})
-
-router.get('/planets/:id/films', (req, res)=> {
-    res.send('Get All Planets')
-})
-
-router.get('/planets/:id/characters', (req, res)=> {
+router.get('/planets/:id/characters', (req, res) => {
     res.send('Get All Planets')
 })
 
